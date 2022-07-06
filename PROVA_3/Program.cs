f75e9f5b5ae0d43759b79054470c3f6d5d9767f6
joaolucastbr@gmail.com <joaolucastbr@gmail.com>
2022-07-06 18:09:12 -0300

    Adicionar arquivos de projeto.

---
+++ PROVA_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROVA_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine(" Números pares de 0 á 100 :");
+            for (i = 1; i < 100; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+            }
+          Console.ReadKey();
+
+        }
+    }
+}
